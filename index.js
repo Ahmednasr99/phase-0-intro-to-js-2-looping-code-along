b4c4d16c0ab2b2be658b8f04658dea4770d2ad1a
Ahmed Nasr <eng_mimo98@yahoo.com>
2022-03-06 05:57:56 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solutions in this file
+
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+let names = ["Guadalupe", "Ollie", "Aki"];
+let event = "surprise";
+function writeCards(names, event) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+      messages.push("Thank you, " + names[i] +", for the wonderful " + event + " gift!")
+    }
+    return messages;
+  }
+let n =10;
+function countDown(n){
+    while (n<=10&&n>=0) {
+    console.log(n);
+    n--;
+    }
+    
+}
